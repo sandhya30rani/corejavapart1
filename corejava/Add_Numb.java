044dc9ecad5994ef3d2c3840a2e5527318bd9a12
sandhya30rani <107548490+sandhya30rani@users.noreply.github.com>
2022-06-28 17:01:17 +0530

    Add files via upload

---
+++ corejava/Add_Numb.java
@@ -0,0 +1,11 @@
+class Add_Numb
+{
+   public static void main(String[] args)
+     {
+       int no1=10;
+       int no2=20;
+       int sum;
+      sum=no1+no2;
+      System.out.println("sum:"+sum);
+}
+}
---
+++ corejava/calculation.java
@@ -0,0 +1,17 @@
+class Calculation{
+public static void main(String[]args)
+{
+int n1=8,n2=4,sum,sub,mul,div;
+sum=n1+n2;
+sub=n1-n2;
+mul=n1*n2;
+div=n1/n2;
+int mod=n1%n2;
+System.out.println("n1=8,n2=4");
+System.out.println("Addition="+sum);
+System.out.println("subtraction="+sub);
+System.out.println("multiplication="+mul);
+System.out.println("Division="+div);
+System.out.println("Mod="+mod);
+}
+}
